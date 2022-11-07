184610b52a39529b029dd9d373f92ff316640469
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2022-11-07 10:13:20 -0800

    Server is running on port 5000 successfully

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const cors = require('cors');
+
+
+const port = process.env.PORT || 5000;
+
+require('dotenv').config()
+const app = express()
+
+
+// middleware
+app.use(cors())
+
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send("Home chef server is running")
+})
+
+
+app.listen(port, () => {
+    console.log("Server is running on port", port);
+})
