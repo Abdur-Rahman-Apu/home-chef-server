430a52fbfe504352a10c76b54cf6daa60ed663e7
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2022-11-07 10:19:07 -0800

    mongodb initial setup is done

--- index.js
+++ index.js
@@ -20,6 +20,22 @@ app.get('/', (req, res) => {
 })
 
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7kbtzra.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+}
+
+run().catch(error => console.log(error))
+
+
 app.listen(port, () => {
     console.log("Server is running on port", port);
 })
